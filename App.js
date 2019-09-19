557adad0fb7859c8adb97899b41c70b31dfba8b3
Diego Partipilo <Diego.Partipilo@weaveability.com>
2019-09-19 11:24:50 +0100

    Implemented onSelect function

--- App.js
+++ App.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Alert, FlatList, SafeAreaView } from 'react-native';
-
-
-let oButtonProps = {
-  title: 'Test',
-  onPress: () => Alert.alert('Button has been pressed')
-}
+import { StyleSheet, Text, View, Button, Alert, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default class App extends Component {
-  state = {
-    data: [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      },
-    ]
+    state = {
+      data: [
+        {
+          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+          title: 'First Item',
+          selected: false
+        },
+        {
+          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+          title: 'Second Item',
+          selected: false
+        },
+        {
+          id: '58694a0f-3da1-471f-bd96-145571e29d72',
+          title: 'Third Item',
+          selected: false
+        }
+      ],
+    }
+  
+  onSelect(id){
+    let data = this.state.data.map((item) => {
+      if (item.id === id ){
+        item.selected = !item.selected;
+      }
+      return item;
+    })
+    this.setState({
+      data: data
+    })
   }
+
   render() {
     return (
     <SafeAreaView style={styles.safeAreaContainer}>
@@ -31,7 +41,13 @@ export default class App extends Component {
         <View style={styles.container}>
           <Text>TO DO LIST</Text>
           <Button style={styles.button} {...oButtonProps} title="Title replacer!"/>
-          <FlatList data={this.state.data} renderItem={({item}) => <Item title={item.title} />}
+          <FlatList data={this.state.data} renderItem={({item}) => (          
+          <Item
+            id={item.id}
+            title={item.title}
+            selected={item.selected}
+            onSelect={this.onSelect.bind(this)}
+          />)}
           keyExtractor={item => item.id}
           />
         </View>
@@ -41,12 +57,23 @@ export default class App extends Component {
   }
 }
 
-const Item = ({ title }) => {
+const Item = ({ id, title, selected, onSelect }) => {
   return (
-    <View style={styles.item}>
+    <TouchableOpacity
+      onPress={() => onSelect(id)}
+      style={[
+        styles.item,
+        { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+      ]}
+    >
       <Text style={styles.title}>{title}</Text>
-    </View>
+    </TouchableOpacity>
   );
+}
+
+let oButtonProps = {
+  title: 'Test',
+  onPress: () => Alert.alert('Button has been pressed')
 }
 
 const styles = StyleSheet.create({
@@ -77,10 +104,11 @@ const styles = StyleSheet.create({
     borderRadius: 5
   },
   item: {
+    flex: 1,
     backgroundColor: '#24C634',
     padding: 10,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    // marginVertical: 8,
+    // marginHorizontal: 16,
   },
   title: {
     fontSize: 22,
